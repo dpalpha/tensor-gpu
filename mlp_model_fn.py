5e22e3f4ea511d129d8019ea41154b0c28d668f6
dpalpha <denys.cherevyk@gmail.com>
2019-07-07 17:56:21 +0200

    Update mlp_model_fn.py

--- mlp_model_fn.py
+++ mlp_model_fn.py
@@ -8,14 +8,11 @@ def model_fit(features, params, mode):
     input_layer = tf.reshape(features["x"], [-1, features["x"].shape[1] ] )
     print ('feature x shape', features["x"].shape)
     print ('reshape shape:', input_layer.shape)
-    #trans = tf.string_to_number(input_layer)
 
     # Dense Layers
     #-------------------------------------------------------------
     hidden1 = tf.layers.dense(inputs=features["x"], units=config['n_hidden1'], activation=tf.nn.relu)
-    
-    training = tf.placeholder_with_default(False, shape=(), name='training')
-    
+        
     bn1 = tf.layers.batch_normalization(hidden1, momentum = 0.9)
 
     drop_h1 = tf.layers.dropout(inputs=bn1, rate=0.3, training=mode == tf.estimator.ModeKeys.TRAIN)
